e90db3d949e1293c8559ae57c795845307674223
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2020-08-30 19:51:13 +0300

    Удалил лишние ссылки на библиотеки

--- server.c
+++ server.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <time.h>
 #include "queue.h"
 #include "sem.h"
 #include "alloc.h"
