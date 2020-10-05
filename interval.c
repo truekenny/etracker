9e436146a22bd829961610acd1fd8d3570d5ba30
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2020-10-05 13:35:41 +0300

    Доработать динамику изменения interval #36

--- interval.c
+++ interval.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "interval.h"
+#include "math.h"
 
-void updateInterval(struct block *block, _Atomic(unsigned int) *interval,
-        unsigned int minInterval, unsigned int maxInterval) {
+void updateInterval(struct block *block, _Atomic (unsigned int) *interval,
+                    unsigned int minInterval, unsigned int maxInterval) {
     double load[3];
 
     if (getloadavg(load, 3) == -1) {
@@ -13,9 +14,15 @@ void updateInterval(struct block *block, _Atomic(unsigned int) *interval,
 
     double maxAllowLoadAverage = ((double) sysconf(_SC_NPROCESSORS_ONLN)) - 0.5;
 
-    unsigned int localInterval = *interval + ((load[2] < maxAllowLoadAverage) ? (-STEP_INTERVAL) : STEP_INTERVAL);
-    localInterval = (localInterval > maxInterval) ? maxInterval : localInterval;
-    localInterval = (localInterval < minInterval) ? minInterval : localInterval;
+    unsigned int localInterval = *interval;
+
+    if (load[0] < maxAllowLoadAverage && load[1] < maxAllowLoadAverage && load[2] < maxAllowLoadAverage)
+        localInterval -= STEP_INTERVAL;
+    else if (load[0] > maxAllowLoadAverage && load[1] > maxAllowLoadAverage && load[2] > maxAllowLoadAverage)
+        localInterval += STEP_INTERVAL;
+
+    localInterval = max(minInterval, localInterval);
+    localInterval = min(maxInterval, localInterval);
 
     if (block != NULL)
         addFormatStringBlock(block, 1000,
