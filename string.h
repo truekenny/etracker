cc521ad2eaee475198e78d74e5075a01ada6fd1e
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2020-10-07 23:18:38 +0300

    Форматирование справки программы
    По ключу --help

--- string.h
+++ string.h
@@ -1,6 +1,26 @@
 #ifndef SC6_STRING_H
 #define SC6_STRING_H
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLD_BLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLD_RED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLD_GREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLD_YELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLD_BLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLD_MAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLD_CYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLD_WHITE   "\033[1m\033[37m"      /* Bold White */
+#define BOLD "\e[1m"            /* Bold */
+#define UNDERLINE "\e[4m"       /* Underline */
+
 _Bool startsWith(const char *start, const char *string);
 
 _Bool endsWith(const char *end, const char *string);
